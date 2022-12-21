323f37ccd3a8a15d0a3d9daf33a3bcf6494889de
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-12-21 16:11:45 -0300

    sdfsdg

--- RPG/R20/scripts/src/index.ts
+++ RPG/R20/scripts/src/index.ts
@@ -1,2 +1,37 @@
-const x: number = 35;
-console.log('test' + x);
+import { readdir, readFile as fsReadFile } from 'fs/promises'
+import { join } from 'path'
+
+// util file funcions
+const listFiles = (...dirs: string[]) =>
+  readdir(join(...dirs), { encoding: 'utf8' })
+
+const readFile = (dir: string, filename: string) =>
+  fsReadFile(join(dir, filename), { encoding: 'utf8' }).then(buff =>
+    buff.toString()
+  )
+
+// business logic functions
+const getTags = (content: string) =>
+  [...content.matchAll(/#(\w+)/gm)].map(([, tagName]) => tagName)
+
+// config
+const baseDir = join(__dirname, '../../')
+const SpellsFolder = join(baseDir, 'Spells')
+const SpellDescriptionsFolder = join(SpellsFolder, 'Spell Descriptions')
+
+// what i want:
+//   - map of all spell names to their tags
+const SpellFileNames = listFiles(SpellDescriptionsFolder)
+
+SpellFileNames.then(filenames =>
+  Promise.all(
+    ['Acid Splash']
+      // filenames
+      // .map(filename => filename.replace(/\.md$/, ''))
+      .map(spellname => readFile(SpellDescriptionsFolder, `${spellname}.md`))
+  )
+)
+  .then(a => a[0])
+  .then(getTags)
+  // .then((a: any) => a[0])
+  .then(console.log)
