98707e335b9e691f2256fbad3b1f8a8e4416ff46
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-08-21 20:14:52 -0300

    auto vault backup: 2025-08-21 20:14:51
    Affected files:
    RPG/R20/scripts/src/compile-spells/index.ts

--- RPG/R20/scripts/src/compile-spells/index.ts
+++ RPG/R20/scripts/src/compile-spells/index.ts
@@ -2,37 +2,38 @@ import { listFiles, writeToFile } from "../file"
 import { makeSpellDefinitionMacroString } from "./makeSpellDefinitionMacroString"
 import { readCsvFile, Spell5eToolsExport } from "./readCsv"
 
-console.log("SDFSDFSDF")
-
 const SpellsRootDir = `../Spells`
 const SpellsListDir = `${SpellsRootDir}/Spell Descriptions`
 const ParsedSpellsOutputDir = `${SpellsListDir}/compiled`
 
 const csvFilenameToParse = `3rd-circle-Spells.csv`
 
-const getAllSpellNames = (spellListDir: string = SpellsListDir) => listFiles(spellListDir)
+const getAllSpellNames = (spellListDir: string) => listFiles(spellListDir)
   .then(spellFilenames => spellFilenames
     .map(spellFilename => spellFilename.replace('.md', ''))
   )
 
-const filterExistingSpells = (spellNames: string[], spells: Spell5eToolsExport[]) => spells.filter(spell => !spellNames.some(spellName => spellName === spell.Name))
+const filterExistingSpells = (spellNames: string[], spells: Spell5eToolsExport[]) => spells
+  .filter(spell => !spellNames.some(spellName => spellName === spell.Name))
 
-const spells = readCsvFile(SpellsRootDir, csvFilenameToParse)
-const spellNames = getAllSpellNames()
-
-const compileNewSpells = (spells: Spell5eToolsExport[], spellNames: string[]) => Promise
-  .all([spells, spellNames])
-  .then(([spells, spellNames]) => {
-    // console.log("before", spells.length)
-    // const afterFilter = filterExistingSpells(spellNames, spells)
-    // console.log("after", afterFilter.length)
-    // return afterFilter
-    return filterExistingSpells(spellNames, spells)
-  })
-  .then(spells => spells
+const compileNewSpells = async (spells: Spell5eToolsExport[], spellNames: string[]) => {
+  const newSpells = filterExistingSpells(spellNames, spells)
     .map(spell => [`${spell.Name}.md`, makeSpellDefinitionMacroString(spell)])
     .map(([filename, content]) => writeToFile(ParsedSpellsOutputDir, filename, content))
-  )
-  .then(fileWrites => Promise.all(fileWrites))
-  .then(value => console.log(value))
 
+  return Promise.all(newSpells)
+    .then(fileWrites => Promise.all(fileWrites))
+    .then(() => spellNames)
+}
+
+const parseSpellsFromCsv = async (csvFileName: string) => {
+  return Promise
+    .all([
+      readCsvFile(SpellsRootDir, csvFileName),
+      getAllSpellNames(SpellsListDir),
+    ])
+    .then(([spells, spellNames]) => compileNewSpells(spells, spellNames))
+    .then(newSpells => console.log("Compiled from csv and wrote to 'compiled' folder the following new spells", newSpells))
+}
+
+parseSpellsFromCsv(csvFilenameToParse)
