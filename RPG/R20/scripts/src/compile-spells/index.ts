21aa7b8ff776b3fd7248165b3ba812de74a58cfe
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-08-21 19:14:48 -0300

    auto vault backup: 2025-08-21 19:14:48
    Affected files:
    RPG/R20/scripts/src/compile-spells/index.ts

--- RPG/R20/scripts/src/compile-spells/index.ts
+++ RPG/R20/scripts/src/compile-spells/index.ts
@@ -1,14 +1,26 @@
+import { listFiles } from "../file"
 import { makeSpellDefinitionMacroString } from "./makeSpellDefinitionMacroString"
 import { readCsvFile } from "./readCsv"
 
 console.log("SDFSDFSDF")
 
-const csvPath = `../Spells/3rd-circle-Spells.csv`
+const SpellsRootDir = `../Spells`
+const SpellsListDir = `${SpellsRootDir}/Spell Descriptions`
 
-readCsvFile(csvPath)
-  .then(spells => spells
-    .map(makeSpellDefinitionMacroString)
-  )
-  // .then(a => console.log(a[0]))
-  // .then
-  .then(a => console.log(a))
+const csvFilenameToParse = `3rd-circle-Spells.csv`
+
+const parseCsvFile = (...csvPath: string[]) =>
+  readCsvFile(...csvPath)
+    .then(spells => spells
+      .map(makeSpellDefinitionMacroString)
+    )
+
+// .then(a => console.log(a[0]))
+// .then
+
+const getAllSpellNames = (spellListDir = SpellsListDir) => listFiles(spellListDir).then(spellFilenames => spellFilenames.map(spellFilename => spellFilename.replace('.md', '')))
+
+// parseCsvFile(SpellsRootDir, csvFilenameToParse).then(a => console.log(a))
+
+
+getAllSpellNames().then((a) => console.log(a))
