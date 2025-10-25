5938b31b96fd912ada1caf87e27059fd7828fa44
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-10-24 23:29:39 -0300

    auto vault backup: 2025-10-24 23:29:39
    Affected files:
    _scripts/test.js

---
+++ _scripts/test.js
@@ -0,0 +1,6 @@
+module.exports = async (params) => {
+  const { quickAddApi, app, variables, obsidian, abort } = params;
+
+  variables.oldStatus = "X"
+  variables.test123 = JSON.stringify(variables)
+};
