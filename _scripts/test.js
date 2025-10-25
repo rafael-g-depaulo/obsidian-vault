2574aa3d87543c632ea6355a2eb15a6dc5cdecf5
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2025-10-24 23:44:59 -0300

    auto vault backup: 2025-10-24 23:44:59
    Affected files:
    .obsidian/plugins/quickadd/data.json
    Journal/25-10-21.md
    _scripts/test.js

--- _scripts/test.js
+++ _scripts/test.js
@@ -1,6 +1,32 @@
-module.exports = async (params) => {
+const script = async (params, settings) => {
   const { quickAddApi, app, variables, obsidian, abort } = params;
 
-  variables.oldStatus = "X"
+  // Get active editor
+  const activeView = app.workspace.getActiveViewOfType(obsidian.MarkdownView);
+  if (!activeView) return
+
+  const editor = activeView.editor;
+  const selection = editor.getSelection()
+
+  variables.selection = selection
+
+  const taskRegex = /-\[(<status>?:.)\] (<task>?:.*)/
+
+  variables.result = taskRegex.exec(variables.selection)
   variables.test123 = JSON.stringify(variables)
-};
+}
+
+module.exports = {
+  entry: script,
+  settings: {
+    name: "Extract Task",
+    options: {
+      "Available Statuses": {
+        type: "text",
+        defaultValue: [' ', 'x', '/'].join(","),
+        description: "comma separated, pls"
+      }
+    }
+  }
+}
+
